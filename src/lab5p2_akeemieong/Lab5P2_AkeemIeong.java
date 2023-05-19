f16ee6f8f8ee2d8e1eabe26f30240e655b4bdee2
Akeem Ieong Ham <akeemieong@gmail.com>
2023-05-19 13:24:03 -0600

    Primer commit

---
+++ src/lab5p2_akeemieong/Lab5P2_AkeemIeong.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab5p2_akeemieong;
+
+/**
+ *
+ * @author SURFACEB2I7
+ */
+public class Lab5P2_AkeemIeong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
